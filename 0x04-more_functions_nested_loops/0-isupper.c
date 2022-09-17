41dd82a1c016dd544bbc9768b354bbd65d260dbc
david-learner-cpu <davidgaruba23@gmail.com>
2022-09-17 06:27:40 -0700

    0m

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - checks if a character is uppercase or not
  * @c: character to be tested
@@ -11,5 +12,6 @@ int _isupper(int c)
 	{
 		return (1);
 	}
+
 	return (0);
 }
